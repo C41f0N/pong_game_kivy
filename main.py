6a5dac9b47f8b34d5b5a3a5bbb1fecad227a9927
Sarim Ahmed <C41f0N@protonmail.com>
2024-06-01 12:39:26 +0500

    ball moving and bouncing

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
 
+from random import randint
 
 
 class PongGame(Widget):
-    pass
+    ball = ObjectProperty(None)
+
+    def serve_ball(self):
+        self.ball.center = self.center
+        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+
+    def update(self, dt):
+        self.ball.move()
+
+        if self.ball.x < 0 or self.ball.right > self.width:
+            self.ball.velocity_x *= -1
+        
+        if self.ball.y < 0 or self.ball.top > self.height:
+            self.ball.velocity_y *= -1
+
+
 
 class PongApp(App):
     def build(self):
-        return PongGame()
+        game = PongGame()
+        game.serve_ball()
+        
+        Clock.schedule_interval(game.update, 1/60)
+
+        return game
+
 
 class PongBall(Widget):
     velocity_x = NumericProperty(0)
@@ -19,7 +42,7 @@ class PongBall(Widget):
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
     def move(self):
-        self.pos += Vector(*self.velocity)
+        self.pos = Vector(*self.velocity) + self.pos
 
 if __name__ == "__main__":
     PongApp().run()
